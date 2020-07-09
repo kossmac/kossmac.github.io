45606409d4dedde89c4aeb1feb89fe4d12837973
Bojana Bogdanov <bojana.bogdanov@mail.schwarz>
2020-07-10 01:04:54 +0200

    oops

---
+++ build/precache-manifest.a574a4d0ee4cad4e87c64d52729ef1dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f4ef0abc5e8346d627f20da971dc2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2724211d0c7901e34e4",
+    "url": "/static/css/main.ed4f2ff3.chunk.css"
+  },
+  {
+    "revision": "c8dfb6afa753deeb8567",
+    "url": "/static/js/2.a50f6fe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a50f6fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2724211d0c7901e34e4",
+    "url": "/static/js/main.c89abecc.chunk.js"
+  },
+  {
+    "revision": "22637b66679dbd34d388",
+    "url": "/static/js/runtime-main.17e905dc.js"
+  },
+  {
+    "revision": "a67d5ec026e87e207421f10ec0829557",
+    "url": "/static/media/metronome.a67d5ec0.svg"
+  }
+]);
